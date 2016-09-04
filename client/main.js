3ebdd38b28cb5c0e16c0fd547bc96d92d2160975
mavericks9 <susantojason9@gmail.com>
2016-09-05 01:56:18 +0800

    updet lagi yow bro

--- client/main.js
+++ client/main.js
@@ -5,7 +5,7 @@ import './main.html';
 
 Template.hello.onCreated(function helloOnCreated() {
   // counter starts at 0
-  this.counter = new ReactiveVar(0);
+    this.counter = new ReactiveVar(0);
 });
 
 Template.hello.helpers({
@@ -15,8 +15,16 @@ Template.hello.helpers({
 });
 
 Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
+  'click #button'(event, instance) {
+    // TAMBAH SKALI
     instance.counter.set(instance.counter.get() + 1);
-  },
+    },
+  'click #buttonMinus'(event, instance) {
+    // KURANGIN
+    instance.counter.set(instance.counter.get() - 1);
+    },
+    'click #buttonReset'(event, instance) {
+    // ENOLIN/RESET
+    instance.counter.set(0);
+    },
 });
