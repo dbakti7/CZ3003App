a5a28061174369c176841c0ef891e302c6775e41
Dian Bakti <dbakti1605@gmail.com>
2016-09-08 13:09:16 +0800

    Added meteor data npm

--- client/home.jsx
+++ client/home.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
-
-export default React.createClass({
+import React, { Component, PropTypes } from 'react';
+import ReactDOM from 'react-dom';
+import { createContainer } from 'meteor/react-meteor-data';
+class Home extends Component {
   render() {
     return <div>Home page goes here</div>
   }
-})
+}
+
+export default createContainer(() => {
+  return {
+    
+  };
+}, Home);
