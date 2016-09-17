1fb4e5d6a018288b023eaba607bbf7ceed741c16
Dian Bakti <dbakti1605@gmail.com>
2016-09-17 16:00:33 +0800

    Separate view and edit page for incident reports

--- client/main.jsx
+++ client/main.jsx
@@ -9,7 +9,8 @@ import Home from '../imports/ui/home.jsx'; // home page
 import User from '../imports/ui/user.jsx'; // user profile page, for create, edit and view
 import Map from '../imports/ui/map.jsx'; // map view
 import Category from '../imports/ui/category.jsx'; // to list out type of crisis, to subscribe
-import Report from '../imports/ui/reports.jsx'; // crisis report page, for create, edit and view
+import Report_Edit from '../imports/ui/reports_edit.jsx'; // crisis report page, for create, edit and view
+import Report_View from '../imports/ui/reports_view.jsx'; // crisis report page, for create, edit and view
 import '../imports/startup/accounts-config.js'; // for account configuration
 
 Meteor.startup(() => {
@@ -19,7 +20,8 @@ Meteor.startup(() => {
     <Route path="/user/:user_id/:edit" component={User}/>
     <Route path="/map" component={Map}/>
     <Route path="/category/:incidentType_id/:edit" component={Category}/>
-    <Route path="/report/:report_id/:edit" component={Report}/>
+    <Route path="/report/:report_id/:edit" component={Report_Edit}/>
+    <Route path="/reports/view" component={Report_View}/>
   </Router>
 ), document.getElementById('container'));
 
--- imports/ui/reports_edit.jsx
+++ imports/ui/reports_edit.jsx
@@ -8,7 +8,8 @@ import {IndexLink, Link } from 'react-router'
 import TrackerReact from 'meteor/ultimatejs:tracker-react';
 import {IncidentType_db} from '../api/incidentType.js';
 
-class Reports extends TrackerReact(React.Component) {
+
+class Reports_Edit extends TrackerReact(React.Component) {
   constructor() {
       super();
       const reportSubscription = Meteor.subscribe('reports', {onReady: function() {
@@ -40,8 +41,6 @@ class Reports extends TrackerReact(React.Component) {
         rSubscription: reportSubscription,
         uSubscription: userSubscription,
         incidentTypeSubscription: incidentTypeSubscription,
-        reportItemLoaded: false,
-        dataLoaded: false,
       }
     }
 
@@ -88,11 +87,6 @@ class Reports extends TrackerReact(React.Component) {
       ReactDOM.findDOMNode(this.refs.textLocation).value = report.location;
       ReactDOM.findDOMNode(this.refs.textAreaDescription).value = report.description;
       ReactDOM.findDOMNode(this.refs.incidentType).value = report.incidentType_id;
-      // ReactDOM.findDOMNode(this.refs.textTitle).disabled = true;
-      // ReactDOM.findDOMNode(this.refs.textLocation).disabled = true;
-      // ReactDOM.findDOMNode(this.refs.textAreaDescription).disabled = true;
-    //  return this.props.report_item.map((report_item) => {
-    //    return (<h1>{report_item._id}</h1>)});
     }
     else {
         var textRB = ReactDOM.findDOMNode(this.refs.textReportedBy);
@@ -104,27 +98,11 @@ class Reports extends TrackerReact(React.Component) {
     }
   }
 
-  // componentWillReceiveProps() {
-  //     ReactDOM.findDOMNode(this.refs.textReportedBy).value = Meteor.user().username;
-  // }
   render() {
-    var userDataAvailable = true;
     var currentUser = this.props.currentUser;
     var reportedByUser = this.props.reportedByUser;
     var report_item = this.props.report_item;
-    if(currentUser == undefined || (report_item.length > 0 && reportedByUser.length == 0) || report_item == undefined) {
-      userDataAvailable = false;      
-    }
-    var loggedOut = (!currentUser && userDataAvailable);
-    var loggedIn = (currentUser && userDataAvailable);
-    // if(this.props.report_item != undefined) {
-    //     this.setState({
-    //       reportItemLoaded : (report_item != undefined)
-    //     });
-    // }
-    // if(this.state.reportItemLoaded) {
-    //   reportedByUser = report_item.length > 0 ? Meteor.users.find({_id: report_item[0].reportedBy}).fetch() : []
-    // }
+    
     return (<div>
       <h2>Report Page</h2> 
     <form name="reportCase" onSubmit={this.handleSubmit.bind(this)} >
@@ -143,44 +121,33 @@ class Reports extends TrackerReact(React.Component) {
           </select><br/>
             <input width="50%" type="submit" value="Report"/>
           </form>
-          <ul>
-          {this.state.ready ? this.renderReports() : null} 
-          </ul>
           {this.state.ready ? this.renderReportItem() : null}
+          <Link to =  "/reports/view" activeClassName="active">Back to list of reports</Link>
           </div>)
           
   }
 }
-// <li><Link to =  "/" activeClassName="active">Index</Link></li>
-//           <li><Link to = "/map" activeClassName="active">Map</Link></li>
 
-Reports.propTypes = {
+Reports_Edit.propTypes = {
   reports: PropTypes.array.isRequired,
 };
 
 export default createContainer(({params}) => {
-  // Meteor.subscribe('reports');
-  // Meteor.subscribe('users');
+  
     const new_report = params.report_id == 0
     const report_item = Reports_db.find({_id: params.report_id}).fetch()
-    // this.setState({
-    //   reportItemLoaded : (report_item != undefined)
-    // });
+    
     const reportedByUser = report_item.length > 0 ? Meteor.users.find({_id: report_item[0].reportedBy}).fetch() : []
     const user_list = Meteor.users.find({}).fetch();
     const currentUser = Meteor.user();
-    const reports = Reports_db.find({}).fetch();
     const incidentTypeList = IncidentType_db.find({}).fetch();
-    // this.setState( {
-    //   dataLoaded : (reportedByUser != undefined) && (user_list != undefined) && (currentUser != undefined) && (reports != undefined)
-    // })
+  
   return {
     new_report,
     report_item,
     reportedByUser,
     user_list,
     currentUser,
-    reports,
     incidentTypeList,
   };
-}, Reports);
+}, Reports_Edit);
