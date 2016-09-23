960e81c1d3fa4fe4c9cb70237b3519c9caca88b0
StupidDummy <kelvinchandra54@gmail.com>
2016-09-24 01:56:16 +0800

    marker

--- imports/ui/map.jsx
+++ imports/ui/map.jsx
@@ -4,16 +4,26 @@ import ReactDOM from 'react-dom';
 import { Meteor } from 'meteor/meteor';
 import reactMixin from 'react-mixin';
 import { TrackerReactMixin} from 'meteor/ultimatejs:tracker-react';
-import {ReactMeteorData} from 'meteor/react-meteor-data';
-
+import {ReactMeteorData, createContainer} from 'meteor/react-meteor-data';
 //React ES6 version
 class App extends React.Component {
+  
   render() {
     return <MyTestMap />;
   }
 }
 
 class MyTestMap extends React.Component {
+  // handleSubmit(event){
+  //   event.preventDefault();
+    
+  //   const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
+
+  //   markerlist.insert({
+  //     text,
+  //     createdAt: new Date(),
+  //   });
+  // }
   componentDidMount() {
     GoogleMaps.load();
   };
@@ -26,23 +36,35 @@ class MyTestMap extends React.Component {
   _mapOptions() {
     return {
        center: new google.maps.LatLng(1.352083,103.819836),
-      //// zoom: 12
+
       //     center : [
       // [-33.890542 , 151.274856,4]],
-      zoom: 2
+      zoom: 12
     };
   };
+  // renderCoors(){
+  //   return this.props.coors.map((coor)=>
+  //     (<maps2 coor={coor} />));
+  // }
   render() {
     if (this.data.loaded)
       return (
               <GoogleMap name="mymap" options={this.data.mapOptions} />
+             
         );
 
     return <div></div>;
   }
 }
 
-
+// MyTestMap.propTypes ={
+//     coors: PropTypes.array.isRequired,
+//   };
+//   export default createContainer(()=>{
+//     return {
+//       coors: Coors.find({}).fetch(),
+//     };
+//   }, MyTestMap);
   reactMixin(MyTestMap.prototype, ReactMeteorData);
 
 class GoogleMap extends React.Component {
@@ -54,7 +76,45 @@ class GoogleMap extends React.Component {
       element: ReactDOM.findDOMNode(this),
       options: this.props.options
     });
-    var markerlist = [[1.352083, 103.819836],[-33.80010128657071, 151.28747820854187]];
+
+    var markerlist = [
+    [1.352083, 103.819836,'Fire'],
+    [1.347582, 103.680699,'Tornado'],
+    [1.297051,103.776402,'Flood']];
+    // var markerlist = [
+    // [1.352083, 103.819836,'Fire','Bishan Fire Station','114 Windsor Park Rd, Singapore 574178'],
+    // [1.347582, 103.680699,'Tornado','Singapore Civil Defence Forces','Lee Wee Nam Library'],
+    // [1.297051,103.776402,'Flood','Singapore Civil Defence Forces','National University of Singapore']];
+    var icons = {
+      Fire: {//fire
+        icon: {
+        url: 'http://www.ifssgroup.com/wp-content/uploads/2016/01/fire-icon-287x300.png',
+        scaledSize: new google.maps.Size(20,20),
+        origin: new google.maps.Point(0,0),
+        anchor: new google.maps.Point(0,0)},
+      },
+      Flood: {//flood
+        icon: {
+        url: 'http://www.covervillemedia.com/statewidesolutions/wp-content/uploads/2015/06/flood-icon.png',
+        scaledSize: new google.maps.Size(20,20),
+        origin: new google.maps.Point(0,0),
+        anchor: new google.maps.Point(0,0)},
+      },
+      Earthquake: {//earthquake
+        icon: {
+        url: 'http://www.tinepal.org/tmp-content/uploads/2015/04/main_offering_service_icon_remodel_earthquake.png',
+        scaledSize: new google.maps.Size(20,20),
+        origin: new google.maps.Point(0,0),
+        anchor: new google.maps.Point(0,0)},
+      },
+      Tornado: {//earthquake
+        icon: {
+        url: 'https://emergency.unl.edu/images/icons-master-101116_03.png',
+        scaledSize: new google.maps.Size(20,20),
+        origin: new google.maps.Point(0,0),
+        anchor: new google.maps.Point(0,0)},
+      }
+    }
     GoogleMaps.ready(this.props.name, function(map) {
       // var marker = new google.maps.Marker({
       //   position: map.options.center,
@@ -63,10 +123,32 @@ class GoogleMap extends React.Component {
     // var i,marker;
     var i;
     for(i = 0; i<markerlist.length; i++){
+      // var contentString = '<div id="content">'+
+      //       '<div id="siteNotice">'+
+      //       '</div>'+
+      //       '<h3 id="firstHeading" class="firstHeading">'+ markerlist[i][4] +'</h1>'+
+      //       '<div id="bodyContent">'+
+      //       '<p> Accident :'+ markerlist[i][2] +'</p>'+
+      //       '<p> Handle By:'+ markerlist[i][3]+
+      //       '</div>'+
+      //       '</div>';
+
+      // var infowindow= new google.maps.InfoWindow({
+      //   content: contentString
+      // });
+      var x = Math.floor((Math.random()*3)+1);
       var  marker = new google.maps.Marker({
           position: new google.maps.LatLng(markerlist[i][0],markerlist[i][1]),
-          map: map.instance
+          map: map.instance,
+          animation: google.maps.Animation.DROP,
+          icon: icons[markerlist[i][2]].icon,
+          title: markerlist[i][2],
+
         });
+      // marker.addListener('click', function() {
+      //     infowindow.open(map, marker);
+      //   });
+
       };
     });
   };
