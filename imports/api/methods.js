bc56ea38917a1ee343168f391c1257de39ee0c7f
dbakti7 <dbakti1605@gmail.com>
2016-11-07 02:59:50 +0800

    intergration

--- imports/api/methods.js
+++ imports/api/methods.js
@@ -19,13 +19,7 @@ var T = new Twit({
  
 var graph = require('fbgraph')
  
-var wallPost = {
-  message: "Life isn't about finding yourself. Life is about creating yourself",
-  privacy: {value: "EVERYONE"},
-  // message: "We do not remember days, we remember moments",
-  // caption: 'This is my wall post example',
-  link: 'http://www.brainyquote.com/quotes/quotes/g/georgebern109542.html?src=t_life',
-};
+
  
 // access token page: EAACEdEose0cBAKZBGe1MZBBITNhaUkEZCHrDO26WkRmxrYMIK3iRkl5BeQ121JbO60MlJ6tm2qFcFc1joJjqDyPgSvlMisluJnxTyVTzE5WdJWDLZBWZCdQPc0gmCnlZCWw8snQFKKX97qvBLKiZBl5gHa1aZCxuJ0DWoEvxT9ZCzZCBbEv5TiIBQI
 // access token user: EAAFd7VBDCW8BALEhRl0ke94mPKKkivs2ZCQqT3LRH691aLIDIi3zblDOfUt5ZCY6QxSv8QePlr8L87wmqTtdcJdx5GqhYbzrxZBtr58R7J7rpYFTGgyZBWWsbnXSTtaVbjIwHOpZAqXuJZCDnJ7RyXlL1NURxVZAhsZD
@@ -135,6 +129,13 @@ Meteor.methods({
     Roles.setUserRoles( userId, role);
   },
   'postToFacebook': function(text) {
+    var wallPost = {
+      message: text,
+      privacy: {value: "EVERYONE"},
+      // message: "We do not remember days, we remember moments",
+      // caption: 'This is my wall post example',
+      link: 'https://www.facebook.com/gbbpentium?fref=ts'
+    };
     if(Meteor.user()) {
       graph.setAccessToken("EAAFd7VBDCW8BALEhRl0ke94mPKKkivs2ZCQqT3LRH691aLIDIi3zblDOfUt5ZCY6QxSv8QePlr8L87wmqTtdcJdx5GqhYbzrxZBtr58R7J7rpYFTGgyZBWWsbnXSTtaVbjIwHOpZAqXuJZCDnJ7RyXlL1NURxVZAhsZD");
       graph.post('/feed',wallPost,function(err,result) {
--- server/main.js
+++ server/main.js
@@ -3,3 +3,7 @@ import '../imports/api/userData.js';
 import '../imports/api/server/publications.js'
 import '../imports/api/methods.js'
 import '../imports/api/incidentType.js'
+import {Meteor} from 'meteor/meteor'
+// Meteor.setInterval(function() {
+//     console.log("INSIDE INTERVAL")
+// }, 2000);
