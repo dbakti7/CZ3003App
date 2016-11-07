ea12609c598eefab40cf672863d001547c28f33b
cacad101 <gbbpentium@gmail.com>
2016-11-07 23:45:08 +0800

    Tables
    Add table for user, category and report view

--- imports/ui/reports_view.jsx
+++ imports/ui/reports_view.jsx
@@ -7,10 +7,13 @@ import { Meteor } from 'meteor/meteor';
 import {IndexLink, Link } from 'react-router'
 import TrackerReact from 'meteor/ultimatejs:tracker-react';
 import {IncidentType_db} from '../api/incidentType.js';
+import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
+
 
 class Reports_View extends TrackerReact(React.Component) {
   constructor() {
       super();
+      var self = this;
       const reportSubscription = Meteor.subscribe('reports', {onReady: function() {
         this.setState({
           
@@ -30,15 +33,32 @@ class Reports_View extends TrackerReact(React.Component) {
       <Report key={reports._id} report={reports} />
     ));
   }
+  
 
+  editFormatter(cell, row){
+    // console.log("HAHAHA",cell);
+    return  <div>
+              <Link to = {`/report/${cell}/0`} activeClassName="active"><button >Edit</button></Link>
+              <button className="delete" onClick={function() {
+                Reports_db.remove(cell);
+              }} >Delete</button>
+            </div>;
+  }
   
   render() {
+    // console.log("ASS", this.props.reports);
+    //  {this.state.ready ? this.renderReports() : null}
+    reportData = this.props.reports;
     
     return (<div>
-          <ul>
-          {this.state.ready ? this.renderReports() : null}
-          </ul> 
-          </div>)
+              <BootstrapTable data={reportData} striped={true} hover={true} pagination={true} search={true}>
+                <TableHeaderColumn dataField="_id" isKey={true} hidden={true}>ID</TableHeaderColumn>
+                <TableHeaderColumn dataField="title" dataSort={true}>Title</TableHeaderColumn>
+                <TableHeaderColumn dataField="locationName" dataSort={true}>Location</TableHeaderColumn>
+                <TableHeaderColumn dataField="status" dataSort={true}>Status</TableHeaderColumn>
+                <TableHeaderColumn dataField="_id" dataFormat={this.editFormatter}>Action</TableHeaderColumn> 
+              </BootstrapTable>
+            </div>)
           
   }
 }
