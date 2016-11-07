4a61bb0d584ec37c2f780009976b78160c3ee2a1
dbakti7 <dbakti1605@gmail.com>
2016-11-07 22:21:54 +0800

    Fixed bugs

--- imports/ui/user.jsx
+++ imports/ui/user.jsx
@@ -33,6 +33,8 @@ class User extends Component {
  
     // Find the text field via the React ref
     const userName = ReactDOM.findDOMNode(this.refs.textUserName).value.trim();
+    if(this.props.newUser)
+      var password = ReactDOM.findDOMNode(this.refs.textPassword).value.trim();
     const fullName = ReactDOM.findDOMNode(this.refs.textFullName).value.trim();
     const email = ReactDOM.findDOMNode(this.refs.textEmail).value.trim();
     const type = ReactDOM.findDOMNode(this.refs.textType).value;
@@ -43,12 +45,19 @@ class User extends Component {
     else
       ReactDOM.findDOMNode(this.refs.AgencyDiv).hidden = false;
       
-    Meteor.call('setRole', Meteor.userId(), type);
-    // console.log(Roles.userIsInRole( Meteor.userId(), 'Admin' ))
-    // console.log(Roles.userIsInRole( Meteor.userId(), 'Operator' ))
-    // console.log(Roles.userIsInRole( Meteor.userId(), 'Agency' ))
+    if(this.props.newUser) {
+       Meteor.call('userAux.addUser', userName, password, function(err, result) {
+         Meteor.call('userData.update', result, fullName, email, type, agencyName, function(err1, result1) {
+           Meteor.call('setRole', newUserId, type);
+         })
+       });
+     }
+     else {
+       Meteor.call('userData.update', Meteor.userId(), fullName, email, type, agencyName);
+       Meteor.call('setRole', Meteor.userId(), type);
+     }
     
-    Meteor.call('userData.update', Meteor.userId(), fullName, email, type, agencyName);     
+         
     //alert("User data has been updated!");
     Bert.alert( 'User data has been updated!', 'success', 'fixed-top', 'fa-check' );
     // Clear form
@@ -103,10 +112,10 @@ class User extends Component {
 
   componentWillReceiveProps() {
     var users_data = UserData_db.find({originalUserId: Meteor.userId()}).fetch();
-
-    ReactDOM.findDOMNode(this.refs.textUserName).value = Meteor.user().username;
-    ReactDOM.findDOMNode(this.refs.textUserName).disabled = true;
-    if (users_data.length > 0) { 
+    if(!this.props.newUser)
+       ReactDOM.findDOMNode(this.refs.textUserName).value = Meteor.user().username;
+     // ReactDOM.findDOMNode(this.refs.textUserName).disabled = !this.props.newUser;
+     if (users_data.length > 0 && !this.props.newUser) { 
       if (users_data[0].email != undefined)
         ReactDOM.findDOMNode(this.refs.textEmail).value = users_data[0].email;
       if (users_data[0].fullName != undefined)
@@ -145,12 +154,17 @@ class User extends Component {
                 <table width="100%">
                   <tr>
                     <td width="30%">UserName:</td>
-                    <td><input type="text" ref="textUserName" value={Meteor.username}/><br/></td>
+                    <td><input type="text" ref="textUserName" disabled={!this.props.newUser} placeholder={this.props.newUser ? "UserName" : ""}/><br/></td>
                   </tr>
                   <tr>
                     <td>FullName:</td>
                     <td><input type="text" ref="textFullName" placeholder="Full Name"/><br/></td>
                   </tr>
+                  {this.props.newUser ? 
+                   <tr>
+                     <td>Password:</td>
+                     <td><input type="text" ref="textPassword" placeholder="Password"/><br/></td>
+                   </tr> : null}
                   <tr>
                     <td>Email:</td> 
                     <td><input type="text" ref="textEmail" placeholder="Email Address"/><br/></td>
@@ -202,9 +216,11 @@ export default createContainer(({params}) => {
   Meteor.subscribe('userData');
   const user_data = UserData_db.find({originalUserId: Meteor.userId()}).fetch();
   const currentUser = Meteor.user();
+  const newUser = (params.user_id == 0)
   return {
         //report_item,
         user_data,
         currentUser,
+        newUser,
       };
 }, User);
